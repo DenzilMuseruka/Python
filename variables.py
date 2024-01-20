c6a7981eb1ba314c907770acccd60c9cbcc6c14c
Denzil Museruka <denzilmuseruka@outlook.com>
2024-01-20 23:49:14 +0200

    get user input from console

--- variables.py
+++ variables.py
@@ -4,3 +4,12 @@ z = 'hello'
 print(type(x)) # outputs: <class 'bool'>       # assign variable z the value of y
 
 print(x, y, z)
+
+name = input('Enter your name:')
+name = input()
+print('Hello...............',name)
+
+# calculator basics
+first_number = int(input('Type the first number: '));
+second_number = int(input('Type the second number: '));
+print("The sum is: ", first_number + second_number)
